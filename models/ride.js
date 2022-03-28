5c31e8a4e723ebd2935bebf4071cb2747dfa795e
illusion <dhruvtiwari110058@gmail.com>
2022-03-28 09:58:21 +0530

    hain bhai changes

--- models/ride.js
+++ models/ride.js
@@ -1,41 +1,43 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const reqString = {
-    type: String,
-    required: true
-
-}
-const rideSchema = new Schema({
+  type: String,
+  required: true,
+};
+const rideSchema = new Schema(
+  {
     source: {
-        place_name: reqString,
-        place_cord: {
-            type: [Number, Number],
-            required: true
-        }
+      place_name: reqString,
+      place_cord: {
+        type: [Number, Number],
+        required: true,
+      },
     },
     destination: {
-        place_name: reqString,
-        place_cord: {
-            type: [Number, Number],
-            required: true
-        }
+      place_name: reqString,
+      place_cord: {
+        type: [Number, Number],
+        required: true,
+      },
     },
     timing: reqString,
     total_rides: {
-        type: [Number],
-        required: true
+      type: [Number],
+      required: true,
     },
     ride_status: {
-        type: [Number],
-        required: true,
+      type: [Number],
+      required: true,
     },
     start_date: {
-        type: Date
+      type: Date,
     },
     end_date: {
-        type: Date
-    }
-}, { timestamps: true })
+      type: Date,
+    },
+  },
+  { timestamps: true }
+);
 
-const Ride = mongoose.model('Ride', rideSchema);
+const Ride = mongoose.model("Ride", rideSchema);
 module.exports = Ride;
--- routers/ridestatus.js
+++ routers/ridestatus.js
@@ -1,32 +1,31 @@
-const express = require('express')
-const User = require('../models/user.js');
-const completedRide = async(req, res) => {
-    const phone = req.query.phone;
-    const ride_id = req.query.id;
-    const date = req.query.date;
+const express = require("express");
+const User = require("../models/user.js");
+const completedRide = async (req, res) => {
+  const phone = req.query.phone;
+  const ride_id = req.query.id;
+  const date = req.query.date;
 
-    try {
-        const user = await User.find({ phone });
-        for (let i = 0; i < user[0].ride.length; i++) {
-            if (user[0].ride[i]._id == ride_id) {
-                for (let j = 0; j < user[0].ride[i].total_rides.length; j++) {
-                    if (user[0].ride[i].total_rides[j] == date) {
-                        user[0].ride[i].ride_status[j] = 1;
-                        break;
-                    }
-                }
-            }
+  try {
+    const user = await User.findOne({ phone });
+    // User.findByIdAndUpdate({ phone, "ride._id": ride_id }, {});
+    for (let i = 0; i < user[0].ride.length; i++) {
+      if (user[0].ride[i]._id == ride_id) {
+        for (let j = 0; j < user[0].ride[i].total_rides.length; j++) {
+          if (user[0].ride[i].total_rides[j] == date) {
+            user[0].ride[i].ride_status[j] = 1;
+            break;
+          }
         }
-        await user[0].save();
-
-        // res.send(arr);
-        res.sendStatus(201);
-
-    } catch (err) {
-        res.status(500).send(err);
+      }
     }
+    await user[0].save();
 
-}
+    // res.send(arr);
+    res.sendStatus(201);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+};
 const router = new express.Router();
 
 router.post("/completedRide", completedRide);
