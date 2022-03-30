8c642f923e11f27ae16f6d7869738c455585372e
Vivaswat Sinha <vivaswatsinha@Vivaswats-MacBook-Pro.local>
2022-03-30 12:52:45 +0530

    Invoice generation

--- src/routers/createPdf.js
+++ src/routers/createPdf.js
@@ -6,16 +6,79 @@ const fs = require("fs");
 const path = require("path");
 const User = require("../models/user.js");
 
-const generateInvoice = async (doc, data) => {
-  doc.fontSize(30).text("Invoice");
 
+function generateHeader(doc) {
   doc
-    .fontSize(15)
-    .text(`Invoice Number: ${data._id}`)
-    .text(`Invoice Date: ${new Date().toLocaleDateString()}`)
-    .text(`Amount Paid: ${data.price}`)
+    // .image("logo.png", 50, 45, { width: 50 })
+    .fillColor("#444444")
+    .fontSize(20)
+    .text("Uber Everyday", 50, 57)
+    .fontSize(10)
+    .text("Uber Everyday", 200, 50, { align: "right" })
+    .text("123 New Delhi", 200, 65, { align: "right" })
+    .text("Delhi, India, 110092", 200, 80, { align: "right" })
     .moveDown();
+}
+
+function generateCustomerInformation(doc, invoice) {
+  doc
+    .fillColor("#444444")
+    .fontSize(20)
+    .text("Invoice", 50, 160);
+
+  generateHr(doc, 185);
+
+  const customerInformationTop = 200;
+  const date = new Date().toLocaleDateString();
+  doc
+    .fontSize(10)
+    .text("Invoice Number:", 50, customerInformationTop)
+    .font("Helvetica-Bold")
+    .text(invoice.id, 150, customerInformationTop)
+    .font("Helvetica")
+    .text("Invoice Date:", 50, customerInformationTop + 15)
+    .text(date, 150, customerInformationTop + 15)
+    .text("Balance Due:", 50, customerInformationTop + 30)
+    .text(invoice.bill, 150, customerInformationTop + 30)
+    .font("Helvetica-Bold")
+    .text("Source: ", 50, customerInformationTop + 45)
+    .text(invoice.source.place_name, 150, customerInformationTop + 45)
+    .text("Destination: ", 50, customerInformationTop + 60)
+    .text(invoice.destination.place_name, 150, customerInformationTop + 60)
+    .moveDown();
+
+  generateHr(doc, 280);
+}
+
+
+function generateFooter(doc) {
+  doc
+    .fontSize(10)
+    .text(
+      "Thank you for using our service.",
+      50,
+      300,
+      { align: "center", width: 500 }
+    );
+}
+
+function generateHr(doc, y) {
+  doc
+    .strokeColor("#aaaaaa")
+    .lineWidth(1)
+    .moveTo(50, y)
+    .lineTo(550, y)
+    .stroke();
+}
+const generateInvoice = async (doc, data) => {
+
+  generateHeader(doc);
+  generateCustomerInformation(doc, data);
+  generateFooter(doc);
+
 };
+
+
 
 const getInvoice = async (req, res) => {
   try {
@@ -26,11 +89,9 @@ const getInvoice = async (req, res) => {
     }).populate("ride_info");
     await currentUserRide.calculateBill();
     if (currentUserRide) {
-      let doc = new pdf({ margin: 50 });
+      let doc = new pdf({ size: [595.28, 380], margin: 50 });
       doc.pipe(res);
-
       await generateInvoice(doc, currentUserRide);
-
       doc.end();
     } else {
       res.status(404).send("ride not found!");
