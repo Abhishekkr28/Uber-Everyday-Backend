b310c5c7ed709b0ed9e4ebd25ad6bd17437365b3
Abhishek <abhishekkr.kr28@gmail.com>
2022-03-30 16:37:29 +0530

    env

--- src/database/mongoose.js
+++ src/database/mongoose.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 
 // connect to mongodb
-const dbURI = "mongodb+srv://Tester:admin123@cluster0.5ekkj.mongodb.net/Details?retryWrites=true&w=majority";
 
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then((result) => console.log("Connected to db"))
     .catch(err => console.log(err));
